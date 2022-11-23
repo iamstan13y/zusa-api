3b4a608564585906f2ade5313af5753e215b2239
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:41:56 +0200

    Basic Configurations - I.

---
+++ ZUSA.API/Models/Local/AccountRequest.cs
@@ -0,0 +1,14 @@
+﻿using ZUSA.API.Enums;
+
+namespace ZUSA.API.Models.Local
+{
+    public class AccountRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
