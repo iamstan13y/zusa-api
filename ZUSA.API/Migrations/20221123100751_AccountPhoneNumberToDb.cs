7dceffc4b46a811b10b0b52ef7f1c38abffe9049
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 12:11:36 +0200

    Account->Phone Number DB Migrations.

---
+++ ZUSA.API/Migrations/20221123100751_AccountPhoneNumberToDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZUSA.API.Migrations
+{
+    public partial class AccountPhoneNumberToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "ZAccounts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "ZAccounts");
+        }
+    }
+}
