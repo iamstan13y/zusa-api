587699ecaa87446c31cb3bb6670bed2f67d98f6d
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:41:41 +0200

    Basic Configurations - I.

--- ZUSA.API/Models/Data/Account.cs
+++ ZUSA.API/Models/Data/Account.cs
@@ -9,6 +9,7 @@ namespace ZUSA.API.Models.Data
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
         public string? Password { get; set; }
         public Role Role { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
