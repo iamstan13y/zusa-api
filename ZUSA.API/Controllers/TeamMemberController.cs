f281a7b9a139c235efe10bdd4ed3898d1ca11e8a
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 01:51:30 +0200

    Bulk Upload Team Members implementation.

---
+++ ZUSA.API/Controllers/TeamMemberController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZUSA.API.Models.Local;
+
+namespace ZUSA.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class TeamMemberController : ControllerBase
+    {
+        private readonly ITeamMemberRepository _teamMemberRepository;
+
+        public TeamMemberController(ITeamMemberRepository teamMemberRepository)
+        {
+            _teamMemberRepository = teamMemberRepository;
+        }
+
+        [HttpPost("add-bulk")]
+        public async Task<IActionResult> AddBulkAsync([FromForm] TeamMembersRequest request)
+        {
+            var result = await _teamMemberRepository.AddBulkAsync(request);
+            return result.Success ? Ok(result) : BadRequest(result);
+        }
+    }
+}
