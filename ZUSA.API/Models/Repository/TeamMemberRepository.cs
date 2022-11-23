09761cf89c02cc9ed582a3e38cf3daef75f2415f
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 01:51:34 +0200

    Bulk Upload Team Members implementation.

--- ZUSA.API/Models/Repository/TeamMemberRepository.cs
+++ ZUSA.API/Models/Repository/TeamMemberRepository.cs
@@ -1,12 +1,32 @@
 ﻿using ZUSA.API.Models.Local;
+using ZUSA.API.Services;
 
 namespace ZUSA.API.Models.Repository
 {
     public class TeamMemberRepository : ITeamMemberRepository
     {
-        public Task<Result<string>> AddBulkAsync(TeamMembersRequest request)
+        private readonly AppDbContext _context;
+        private readonly IExcelService _excelService;
+        public TeamMemberRepository(AppDbContext context, IExcelService excelService)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _excelService = excelService;
+        }
+
+        public async Task<Result<string>> AddBulkAsync(TeamMembersRequest request)    
+        {
+            var teamMembers = await _excelService.ExtractRecordsAsync(request.TeamExcelFile!);
+
+            teamMembers.ToList().ForEach(member =>
+            {
+                member.SportId = request.SportId;
+                member.SchoolId = request.SchoolId;
+            });
+            
+            await _context.TeamMembers!.AddRangeAsync(teamMembers);
+            await _context.SaveChangesAsync();
+            
+            return new Result<string>("Team members added successfully");
         }
     }
 }
