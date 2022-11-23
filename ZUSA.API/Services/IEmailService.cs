13a41cb42c1bdbf026a41965cd684cba573c4093
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:45:01 +0200

    Basic Configurations - II.

---
+++ ZUSA.API/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using ZUSA.API.Models.Local;
+
+namespace ZUSA.API.Services
+{
+    public interface IEmailService
+    {
+        Task<Result<string>> SendEmailAsync(EmailRequest email);
+    }
+}
