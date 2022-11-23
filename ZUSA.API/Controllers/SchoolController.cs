f95d930740e6ec3a050d3c778ac7288981b2845d
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 15:35:39 +0200

    Get All Schools Implementation.

---
+++ ZUSA.API/Controllers/SchoolController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZUSA.API.Models.Repository.IRepository;
+
+namespace ZUSA.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SchoolController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public SchoolController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok( await _unitOfWork.School.GetAllAsync());
+         
+    }
+}
