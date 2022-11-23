86f80578a37669a0f10c60a6cd686ebb9d21dabd
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 06:46:26 +0200

    Initial DB Migrations.

---
+++ ZUSA.API/Models/Local/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZUSA.API.Models.Data;
+
+namespace ZUSA.API.Models.Local
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Account>? ZAccounts { get; set; }
+    }
+}
