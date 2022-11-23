ccd2f93783a4317215e640850735ad76910cff9f
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 06:46:19 +0200

    Initial DB Migrations.

---
+++ ZUSA.API/Models/Data/Account.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using ZUSA.API.Enums;
+
+namespace ZUSA.API.Models.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public Role Role { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? Token { get; set; }
+    }
+}
