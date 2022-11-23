ab9b241f5c274fe094a3a4d9705771c7f6d7f9eb
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 00:04:48 +0200

    Team Member Model & DB Migrations.

---
+++ ZUSA.API/Models/Data/TeamMember.cs
@@ -0,0 +1,18 @@
+﻿namespace ZUSA.API.Models.Data
+{
+    public class TeamMember
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string? Gender { get; set; }
+        public string? RegNumber { get; set; }
+        public string? IdNumber { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public int SchoolId { get; set; }
+        public int SportId { get; set; }
+        public Sport? Sport { get; set; }
+        public School? School { get; set; }
+    }
+}
