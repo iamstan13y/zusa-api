2fe8134fc81e89d8e144273a423860da2885abc8
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 00:20:10 +0200

    Team Member Repository Skeleton.

---
+++ ZUSA.API/Models/Repository/TeamMemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using ZUSA.API.Models.Local;
+
+namespace ZUSA.API.Models.Repository
+{
+    public class TeamMemberRepository : ITeamMemberRepository
+    {
+        public Task<Result<string>> AddBulkAsync(TeamMembersRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
