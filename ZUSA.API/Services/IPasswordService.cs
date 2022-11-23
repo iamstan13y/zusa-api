4d8f13b0c3bc2878c863763296dcb988fd8ba788
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:45:12 +0200

    Basic Configurations - II.

---
+++ ZUSA.API/Services/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace ZUSA.API.Services
+{
+    public interface IPasswordService
+    {
+        string HashPassword(string password);
+        bool VerifyHash(string password, string hashedPassword);
+    }
+}
