16466314bdd319b825336a5016c3c42d5c2e006e
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 12:31:17 +0200

    Minor fixes.

---
+++ ZUSA.API/Models/Repository/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using ZUSA.API.Models.Local;
+using ZUSA.API.Models.Repository.IRepository;
+using ZUSA.API.Services;
+
+namespace ZUSA.API.Models.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        //public IEventTypeRepository EventType { get; private set; }
+
+        public UnitOfWork(AppDbContext context)
+        {
+            //EventType = new EventTypeRepository(context);
+            _context = context;
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+    }
+}
