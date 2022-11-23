4694de6ce38f31743aab34c63564bc5ee893b152
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 22:04:28 +0200

    Get All Accounts Paged.

--- ZUSA.API/Models/Repository/IRepository/IAccountRepository.cs
+++ ZUSA.API/Models/Repository/IRepository/IAccountRepository.cs
@@ -1,5 +1,6 @@
 ﻿using ZUSA.API.Models.Data;
 using ZUSA.API.Models.Local;
+using ZUSA.API.Utility;
 
 namespace ZUSA.API.Models.Repository.IRepository
 {
@@ -13,8 +14,9 @@ namespace ZUSA.API.Models.Repository.IRepository
         Task<Result<Account>> VerifyOtpAsync(VerifyOtpRequest request);
         Task<Result<Account>> LoginAsync(LoginRequest request);
         Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePassword);
+        Task<Result<Pageable<Account>>> GetAllPagedAsync(Pagination pagination);
         //Task<Result<string>> GetResetPasswordCodeAsync(string email);
         //Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPassword);
-       // Task<Result<string>> ResendOtpAsync(string email);
+        // Task<Result<string>> ResendOtpAsync(string email);
     }
 }
