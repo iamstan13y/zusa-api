997cc5d76f890cc0eb96222ecd623d4afa8a6f98
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:42:29 +0200

    Basic Configurations - I.

---
+++ ZUSA.API/Models/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq.Expressions;
+using ZUSA.API.Models.Local;
+using ZUSA.API.Utility;
+
+namespace ZUSA.API.Models.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<Result<T>> FindAsync(int id);
+        Task<Result<T>> GetFirstOrDefaultAsync(Expression<Func<T, bool>> filter);
+        Task<Result<IEnumerable<T>>> GetAllAsync();
+        Task<Result<Pageable<T>>> GetAllPagedAsync(Pagination pagination);
+        Task<Result<T>> AddAsync(T entity);
+        Task<Result<bool>> DeleteAsync(T entity);
+        Task<Result<T>> UpdateAsync(T entity);
+    }
+}
