508dd247a0f76a87a1cd036ca88010f820925a2d
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 23:11:42 +0200

    Added Sport Controller.

---
+++ ZUSA.API/Controllers/SportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZUSA.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SportController : ControllerBase
+    {
+
+    }
+}
