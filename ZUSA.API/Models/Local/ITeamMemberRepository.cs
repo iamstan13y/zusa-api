3ba150029a956c23365d0a0efadbf69439f8374d
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 00:20:19 +0200

    Team Member Repository Skeleton.

---
+++ ZUSA.API/Models/Local/ITeamMemberRepository.cs
@@ -0,0 +1,9 @@
+﻿using ZUSA.API.Models.Data;
+
+namespace ZUSA.API.Models.Local
+{
+    public interface ITeamMemberRepository
+    {
+        Task<Result<string>> AddBulkAsync(TeamMembersRequest request);
+    }
+}
---
+++ ZUSA.API/Models/Local/TeamMembersRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ZUSA.API.Models.Local
+{
+    public class TeamMembersRequest
+    {
+        public int SportId { get; set; }
+        public int SchoolId { get; set; }
+        public IFormFile? TeamExcelFile { get; set; }
+    }
+}
