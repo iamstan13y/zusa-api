7663c120ed1fa0ce964d4d6d797b5d44e2e54ebf
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 15:36:01 +0200

    School DB Migrations.

---
+++ ZUSA.API/Migrations/20221123133347_SchoolToDb.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZUSA.API.Migrations
+{
+    public partial class SchoolToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SchoolId",
+                table: "ZAccounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Schools",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schools", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ZAccounts_SchoolId",
+                table: "ZAccounts",
+                column: "SchoolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ZAccounts_Schools_SchoolId",
+                table: "ZAccounts",
+                column: "SchoolId",
+                principalTable: "Schools",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ZAccounts_Schools_SchoolId",
+                table: "ZAccounts");
+
+            migrationBuilder.DropTable(
+                name: "Schools");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ZAccounts_SchoolId",
+                table: "ZAccounts");
+
+            migrationBuilder.DropColumn(
+                name: "SchoolId",
+                table: "ZAccounts");
+        }
+    }
+}
