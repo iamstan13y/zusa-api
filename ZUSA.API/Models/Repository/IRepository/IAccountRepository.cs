5e0ca51d7568d4fcff32d4f8dc9b29158c4a308e
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:42:24 +0200

    Basic Configurations - I.

---
+++ ZUSA.API/Models/Repository/IRepository/IAccountRepository.cs
@@ -0,0 +1,19 @@
+﻿using ZUSA.API.Models.Data;
+using ZUSA.API.Models.Local;
+
+namespace ZUSA.API.Models.Repository.IRepository
+{
+    public interface IAccountRepository
+    {
+        Task<Result<Account>> AddAsync(Account account);
+        Task<Result<Account>> GetByIdAsync(int id);
+        Task<Result<IEnumerable<Account>>> GetAllAsync();
+        Task<Result<Account>> UpdateAsync(Account account);
+        Task<Result<bool>> DeleteAsync(Account account);
+       // Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePassword);
+        //Task<Result<string>> GetResetPasswordCodeAsync(string email);
+        //Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPassword);
+        //Task<Result<Account>> ConfirmAccountAsync(ConfirmAccountRequest confirmAccount);
+       // Task<Result<string>> ResendOtpAsync(string email);
+    }
+}
