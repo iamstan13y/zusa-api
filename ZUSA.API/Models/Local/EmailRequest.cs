1810417d92c18daa6e1d89a777ff953c2a1c8276
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 11:42:09 +0200

    Basic Configurations - I.

---
+++ ZUSA.API/Models/Local/EmailRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ZUSA.API.Models.Local
+{
+    public class EmailRequest
+    {
+        public string? To { get; set; }
+        public string? Subject { get; set; }
+        public string? Body { get; set; }
+    }
+}
