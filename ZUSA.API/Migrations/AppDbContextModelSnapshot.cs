0fc950832875aa9fc0e1b08b18ae7ee4a0d5ae38
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 12:11:45 +0200

    Account->Phone Number DB Migrations.

--- ZUSA.API/Migrations/AppDbContextModelSnapshot.cs
+++ ZUSA.API/Migrations/AppDbContextModelSnapshot.cs
@@ -45,12 +45,37 @@ namespace ZUSA.API.Migrations
                     b.Property<string>("Password")
                         .HasColumnType("nvarchar(max)");
 
+                    b.Property<string>("PhoneNumber")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<int>("Role")
                         .HasColumnType("int");
 
                     b.HasKey("Id");
 
                     b.ToTable("ZAccounts");
+                });
+
+            modelBuilder.Entity("ZUSA.API.Models.Data.GeneratedCode", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
+
+                    b.Property<string>("Code")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<DateTime>("DateCreated")
+                        .HasColumnType("datetime2");
+
+                    b.Property<string>("UserEmail")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("GeneratedCodes");
                 });
 #pragma warning restore 612, 618
         }
