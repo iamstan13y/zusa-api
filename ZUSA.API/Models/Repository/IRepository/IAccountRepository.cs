87fb95fd0c21cdc06a28ec8afe42ca82326a5ba8
Keith Stanley <iamstan13y@gmail.com>
2022-11-23 14:05:48 +0200

    Verify OTP implementation.

--- ZUSA.API/Models/Repository/IRepository/IAccountRepository.cs
+++ ZUSA.API/Models/Repository/IRepository/IAccountRepository.cs
@@ -10,10 +10,10 @@ namespace ZUSA.API.Models.Repository.IRepository
         Task<Result<IEnumerable<Account>>> GetAllAsync();
         Task<Result<Account>> UpdateAsync(Account account);
         Task<Result<bool>> DeleteAsync(Account account);
+        Task<Result<Account>> VerifyOtpAsync(VerifyOtpRequest request);
        // Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePassword);
         //Task<Result<string>> GetResetPasswordCodeAsync(string email);
         //Task<Result<Account>> ResetPasswordAsync(ResetPasswordRequest resetPassword);
-        //Task<Result<Account>> ConfirmAccountAsync(ConfirmAccountRequest confirmAccount);
        // Task<Result<string>> ResendOtpAsync(string email);
     }
 }
