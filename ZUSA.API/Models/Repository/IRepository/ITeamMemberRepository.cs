6a4549e5792d98f92d65d56084c0a04826b059f7
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 11:10:21 +0200

    Get Team Member by Subsription Implementation.

--- ZUSA.API/Models/Repository/IRepository/ITeamMemberRepository.cs
+++ ZUSA.API/Models/Repository/IRepository/ITeamMemberRepository.cs
@@ -6,6 +6,7 @@ namespace ZUSA.API.Models.Repository.IRepository
     public interface ITeamMemberRepository
     {
         Task<Result<string>> AddBulkAsync(TeamMembersRequest request);
+        Task<Result<IEnumerable<TeamMember>>> GetBySubscriptionIdAsync(int subscriptionId);
         Task<Result<IEnumerable<TeamMember>>> GetBySchoolIdAsync(int schoolId);
         Task<Result<IEnumerable<TeamMember>>> GetBySportIdAsync(int sportId);
         Task<Result<IEnumerable<TeamMember>>> GetBySchoolAndSportIdAsync(int schoolId, int sportId);
