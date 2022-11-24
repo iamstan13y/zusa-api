07c97396e2a92d247ac4b681eb53f21e3ac97e14
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 03:10:17 +0200

    Namespace fix.

---
+++ ZUSA.API/Models/Repository/IRepository/ITeamMemberRepository.cs
@@ -0,0 +1,10 @@
+﻿using ZUSA.API.Models.Data;
+using ZUSA.API.Models.Local;
+
+namespace ZUSA.API.Models.Repository.IRepository
+{
+    public interface ITeamMemberRepository
+    {
+        Task<Result<string>> AddBulkAsync(TeamMembersRequest request);
+    }
+}
