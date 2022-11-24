9743ee5219f034c0c664c4cf1c96dfa78c0c049b
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 09:49:43 +0200

    Add Subscription Implementation.

--- ZUSA.API/Controllers/SubscriptionController.cs
+++ ZUSA.API/Controllers/SubscriptionController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ZUSA.API.Models.Data;
+using ZUSA.API.Models.Local;
 using ZUSA.API.Models.Repository.IRepository;
 
 namespace ZUSA.API.Controllers
@@ -32,5 +33,19 @@ namespace ZUSA.API.Controllers
 
         [HttpGet("school/{schoolId}")]
         public async Task<IActionResult> GetBySchool(int schoolId) => Ok(await _unitOfWork.Subscription.GetBySchoolIdAsync(schoolId));
+
+        [HttpPost]
+        public async Task<IActionResult> Post(SubscriptionRequest request)
+        {
+            var result = await _unitOfWork.Subscription.AddAsync(new Subscription
+            {
+                SportId = request.SportId,
+                SchoolId = request.SchoolId
+            });
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        } 
     }
 }
