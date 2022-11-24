e52453b216fb90a90626eebbe7f3c5ea988bcf56
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 09:28:10 +0200

    Added Subscription Controller.

---
+++ ZUSA.API/Controllers/SubscriptionController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZUSA.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class SubscriptionController : ControllerBase
+    {
+    }
+}
