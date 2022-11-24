73b5f5baaa6ff3e04565a685080d108edd106a14
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 09:35:53 +0200

    Subscription Repository Custom methods.

--- ZUSA.API/Models/Repository/IRepository/ISubscriptionRepository.cs
+++ ZUSA.API/Models/Repository/IRepository/ISubscriptionRepository.cs
@@ -1,8 +1,11 @@
 ﻿using ZUSA.API.Models.Data;
+using ZUSA.API.Models.Local;
 
 namespace ZUSA.API.Models.Repository.IRepository
 {
     public interface ISubscriptionRepository : IRepository<Subscription>
     {
+        Task<Result<IEnumerable<Subscription>>> GetBySportIdAsync(int sportId);
+        Task<Result<IEnumerable<Subscription>>> GetBySchoolIdAsync(int schoolId);
     }
 }
