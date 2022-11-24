09b78794e6437b03a723e65f67381cc6e9af18bd
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 09:21:45 +0200

    Subscription Model & DB Migrations.

---
+++ ZUSA.API/Models/Data/Subscription.cs
@@ -0,0 +1,13 @@
+﻿namespace ZUSA.API.Models.Data
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public int SportId { get; set; }
+        public int SchoolId { get; set; }
+        public School? School { get; set; }
+        public Sport? Sport { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; } = true;
+    }
+}
