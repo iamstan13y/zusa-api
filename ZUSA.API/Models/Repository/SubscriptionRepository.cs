b3fa09ff3386445631422d15edc13595a0e410f4
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 09:41:28 +0200

    Get Subscriptions By Sport Implementation.

--- ZUSA.API/Models/Repository/SubscriptionRepository.cs
+++ ZUSA.API/Models/Repository/SubscriptionRepository.cs
@@ -1,4 +1,5 @@
-﻿using ZUSA.API.Models.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ZUSA.API.Models.Data;
 using ZUSA.API.Models.Local;
 using ZUSA.API.Models.Repository.IRepository;
 
@@ -6,8 +7,10 @@ namespace ZUSA.API.Models.Repository
 {
     public class SubscriptionRepository : Repository<Subscription>, ISubscriptionRepository
     {
+        private readonly AppDbContext _context;
         public SubscriptionRepository(AppDbContext context) : base(context)
         {
+            _context = context;
         }
 
         public Task<Result<IEnumerable<Subscription>>> GetBySchoolIdAsync(int schoolId)
@@ -15,9 +18,16 @@ namespace ZUSA.API.Models.Repository
             throw new NotImplementedException();
         }
 
-        public Task<Result<IEnumerable<Subscription>>> GetBySportIdAsync(int sportId)
+        public async Task<Result<IEnumerable<Subscription>>> GetBySportIdAsync(int sportId)
         {
-            throw new NotImplementedException();
+            var subscriptions = await _context.Subscriptions!
+                .Where(s => s.SportId == sportId)
+                .Include(x => x.Sport)
+                .Include(x => x.School)
+                .OrderByDescending(x => x.DateCreated)
+                .ToListAsync();
+
+            return new Result<IEnumerable<Subscription>>(subscriptions);
         }
     }
 }
