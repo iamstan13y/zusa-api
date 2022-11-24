6c6f00fd7a18f26e8eb7a096488335adbb5a75de
Keith Stanley <iamstan13y@gmail.com>
2022-11-24 09:27:09 +0200

    Subscription Repository & UoW.

---
+++ ZUSA.API/Models/Repository/SubscriptionRepository.cs
@@ -0,0 +1,13 @@
+﻿using ZUSA.API.Models.Data;
+using ZUSA.API.Models.Local;
+using ZUSA.API.Models.Repository.IRepository;
+
+namespace ZUSA.API.Models.Repository
+{
+    public class SubscriptionRepository : Repository<Subscription>, ISubscriptionRepository
+    {
+        public SubscriptionRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
