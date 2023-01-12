a85e824e6eb8a3d3056a1e08983c2074616dfd4b
Keith Stanley <iamstan13y@gmail.com>
2023-01-13 00:43:30 +0200

    Add Team Member Implementation.

---
+++ ZUSA.API/Models/Local/TeamMemberRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace ZUSA.API.Models.Local
+{
+    public class TeamMemberRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public string? Gender { get; set; }
+        public string? RegNumber { get; set; }
+        public string? IdNumber { get; set; }
+        public int SubscriptionId { get; set; }
+    }
+}
