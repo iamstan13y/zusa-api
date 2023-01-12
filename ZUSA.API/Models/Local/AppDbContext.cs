2f9c4a7df30bdd46cc55f0edc778fffd5cd5b4d3
Keith Stanley <iamstan13y@gmail.com>
2023-01-12 22:37:08 +0200

    Updated Naming.

--- ZUSA.API/Models/Local/AppDbContext.cs
+++ ZUSA.API/Models/Local/AppDbContext.cs
@@ -10,7 +10,7 @@ namespace ZUSA.API.Models.Local
 
         }
 
-        public DbSet<Account>? ZAccounts { get; set; }
+        public DbSet<Account>? Accounts { get; set; }
         public DbSet<GeneratedCode>? GeneratedCodes { get; set; }
         public DbSet<School>? Schools { get; set; }
         public DbSet<Sport>? Sports { get; set; }
