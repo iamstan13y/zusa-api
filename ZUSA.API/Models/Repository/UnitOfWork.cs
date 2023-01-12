ba5e74da884bb88d284409eb7bf57c2f12408aeb
Keith Stanley <iamstan13y@gmail.com>
2023-01-13 00:43:49 +0200

    Add Team Member Implementation.

--- ZUSA.API/Models/Repository/UnitOfWork.cs
+++ ZUSA.API/Models/Repository/UnitOfWork.cs
@@ -10,12 +10,14 @@ namespace ZUSA.API.Models.Repository
         public ISchoolRepository School { get; private set; }
         public ISportRepository Sport { get; private set; }
         public ISubscriptionRepository Subscription { get; private set; }
+        public ITeamMemberRepository TeamMember { get; private set; }
 
         public UnitOfWork(AppDbContext context)
         {
             School = new SchoolRepository(context);
             Sport = new SportRepository(context);
             Subscription = new SubscriptionRepository(context);
+            TeamMember = new TeamMemberRepository(context);
             _context = context;
         }
 
