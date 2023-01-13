bc6c4f23f95e18d572cb65f4f5c889312fb27acc
Keith Stanley <iamstan13y@gmail.com>
2023-01-13 22:24:57 +0200

    Sport Deadline & DB Migrations.

--- ZUSA.API/Models/Data/Sport.cs
+++ ZUSA.API/Models/Data/Sport.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string? Name { get; set; }
         public int TeamMemberLimit { get; set; }
+        public DateTime Deadline { get; set; }
     }
 }
