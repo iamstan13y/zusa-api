9aae2161b0a53e5998400ff1b53b752b739074e1
Keith Stanley <iamstan13y@gmail.com>
2023-01-15 19:54:39 +0200

    Buggy Code.

---
+++ ZUSA.API/Models/Data/OtpCode.cs
@@ -0,0 +1,10 @@
+﻿namespace ZUSA.API.Models.Data
+{
+    public class OtpCode
+    {
+        public int Id { get; set; }
+        public string? Email { get; set; }
+        public string? Code { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
