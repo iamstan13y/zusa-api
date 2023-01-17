def297fd100429acf89cddca6225d4396510b728
Keith Stanley <iamstan13y@gmail.com>
2023-01-17 23:53:17 +0200

    Get Team Member by Id.

--- ZUSA.API/Controllers/TeamMemberController.cs
+++ ZUSA.API/Controllers/TeamMemberController.cs
@@ -39,6 +39,15 @@ namespace ZUSA.API.Controllers
             return Ok(result);
         }
 
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _unitOfWork.TeamMember.GetByIdAsync(id);
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+
         [HttpPost("bulk")]
         public async Task<IActionResult> AddBulkAsync([FromForm] TeamMembersRequest request)
         {
