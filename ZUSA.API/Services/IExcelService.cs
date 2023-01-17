4c037bbbc53b5cc50908da9dbdda99379819c1b6
Keith Stanley <iamstan13y@gmail.com>
2023-01-18 00:59:01 +0200

    Generate Team Member Excel Config.

--- ZUSA.API/Services/IExcelService.cs
+++ ZUSA.API/Services/IExcelService.cs
@@ -1,9 +1,11 @@
 ﻿using ZUSA.API.Models.Data;
+using ZUSA.API.Models.Local;
 
 namespace ZUSA.API.Services
 {
     public interface IExcelService
     {
         Task<IEnumerable<TeamMember>> ExtractRecordsAsync(IFormFile excelFile);
+        Task<string> GenerateExcelAsync(List<TeamMemberExcelRequest> data);
     }
 }
