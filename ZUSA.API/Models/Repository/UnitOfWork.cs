2e3a0f1b0366f2b8f44785a674621acca25c85fd
Keith Stanley <iamstan13y@gmail.com>
2023-01-18 01:32:24 +0200

    Generate Team Member Excel - By Subscription Id.

--- ZUSA.API/Models/Repository/UnitOfWork.cs
+++ ZUSA.API/Models/Repository/UnitOfWork.cs
@@ -1,23 +1,24 @@
 ﻿using ZUSA.API.Models.Local;
 using ZUSA.API.Models.Repository.IRepository;
+using ZUSA.API.Services;
 
 namespace ZUSA.API.Models.Repository
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly AppDbContext _context;
-
+        
         public ISchoolRepository School { get; private set; }
         public ISportRepository Sport { get; private set; }
         public ISubscriptionRepository Subscription { get; private set; }
         public ITeamMemberRepository TeamMember { get; private set; }
 
-        public UnitOfWork(AppDbContext context)
+        public UnitOfWork(AppDbContext context, IExcelService excelService)
         {
             School = new SchoolRepository(context);
             Sport = new SportRepository(context);
             Subscription = new SubscriptionRepository(context);
-            TeamMember = new TeamMemberRepository(context);
+            TeamMember = new TeamMemberRepository(context, excelService);
             _context = context;
         }
 
