5a65b3aee9a5880436c7c57cd20d0216015befc0
iamstan13y <iamstan13y@gmail.com>
2023-03-25 16:06:16 +0200

    Subscription Gender & DB Migrations.

---
+++ ZUSA.API/Migrations/20230325140226_SubscriptionGenderToDb.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZUSA.API.Migrations
+{
+    public partial class SubscriptionGenderToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "Subscriptions",
+                type: "nvarchar(1)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Subscriptions");
+        }
+    }
+}
