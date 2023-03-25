b20eacf482a199e601ff7a0b88665a785db3dd38
iamstan13y <iamstan13y@gmail.com>
2023-03-25 16:08:18 +0200

    Subscription Gender & DB Migrations.

--- ZUSA.API/Models/Data/Subscription.cs
+++ ZUSA.API/Models/Data/Subscription.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public int SportId { get; set; }
         public int SchoolId { get; set; }
+        public char Gender { get; set; }
         public School? School { get; set; }
         public Sport? Sport { get; set; }
         public DateTime DateCreated { get; set; } = DateTime.Now;
