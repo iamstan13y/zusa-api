e732c8d4aec26bf7f575e04008e736c0104aa505
iamstan13y <iamstan13y@gmail.com>
2023-03-28 14:38:06 +0200

    Reset Password Implementation.

---
+++ ZUSA.API/Models/Local/ResetPasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ZUSA.API.Models.Local
+{
+    public class ResetPasswordRequest
+    {
+        public string? UserEmail { get; set; }
+        public string? OtpCode { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
